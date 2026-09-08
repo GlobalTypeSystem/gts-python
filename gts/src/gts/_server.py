1712a17418cfccaa4a22f96c6b8bbe4a99345986
Artfizer <artifizer@gmail.com>
2026-09-08 22:28:37 +0300

    chore: bump version to 0.13.1
    Signed-off-by: Artfizer <artifizer@gmail.com>

--- gts/src/gts/_server.py
+++ gts/src/gts/_server.py
@@ -185,7 +185,7 @@ class GtsHttpServer:
         self.host = host
         self.port = port
         self.base_url = f"http://{self.host}:{self.port}"
-        self.app = FastAPI(title="GTS Server", version="0.13.0")
+        self.app = FastAPI(title="GTS Server", version="0.13.1")
         self.app.add_middleware(
             _RequestLoggingMiddleware,
             verbose=self.ops.verbose,
